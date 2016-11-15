d43dee53246ca760b7e739541e22b5ea995e14fa
Ted Mader <ted.mader@ge.com>
2016-11-14 18:12:55 -0600

    Update mesh_buffer.hpp and add traingle_buffer.hpp

--- triangle_buffer.hpp
+++ triangle_buffer.hpp
@@ -1,35 +1,36 @@
-#ifndef MESH_BUFFER_H
-#define MESH_BUFFER_H
+#ifndef TRIANGLE_BUFFER_H
+#define TRIANGLE_BUFFER_H
 
 #include <vector>
 #include "point.hpp"
 
 /**
- * Contains function declarations for MeshBuffer objects.
+ * Contains function declarations for TriangleBuffer objects.
  * @author Ted Mader
  * @date 2016-11-14
  */
-class MeshBuffer : public std::vector<Point> {
+class TriangleBuffer : public std::vector<Point> {
 
 public:
   /**
-   * Returns a constructed MeshBuffer object.
+   * Returns a constructed TriangleBuffer object.
    *
    * @param p1 a point
    * @param p2 a point
    * @param p3 a point
    * @return the constructed Buffer
    */
-  MeshBuffer(Point p1, Point p2, Point p3);
+  TriangleBuffer(Point p1, Point p2, Point p3);
 
   /**
    * Adds a edge to the buffer by linking an existing
    * Point to a new Point.
    *
    * @param i an index for an existing point
+   * @param j an index for an existing point
    * @param p a Point to be added
    */
-  void add(int i, Point p);
+  void add(int i, int j, Point p);
 
   /**
    * Modifies the position of a Point by an index by
