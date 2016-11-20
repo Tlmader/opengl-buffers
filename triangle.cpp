1b691c24fdf05cb22233be32380bfd01dbd9aae2
Ted Mader <ted.mader@ge.com>
2016-11-19 22:21:55 -0600

    Remove unnecessary function calls

--- triangle.cpp
+++ triangle.cpp
@@ -6,9 +6,7 @@
  * @date 2016-11-14
  */
 
-Triangle::Triangle(vec4 &a, vec4 &b, vec4 &c) : a(a), b(b), c(c) {
-  calcNormal();
-}
+Triangle::Triangle(vec4 &a, vec4 &b, vec4 &c) : a(a), b(b), c(c) {}
 
 Triangle::Triangle(const Triangle &rhs) : a(rhs.a), b(rhs.b), c(rhs.c) {}
 
@@ -31,8 +29,6 @@ const vec4 Triangle::getNormal() const {
 const vec4 Triangle::calcNormal() const {
   vec4 e1 = b - a;
   vec4 e2 = c - b;
-  printf("%f, %f, %f, %f\n", e1[0], e1[1], e1[2], e1[3]);
-  printf("%f, %f, %f, %f\n", e2[0], e2[1], e2[2], e2[3]);
   vec4 n = {(e1[1] * e2[2]) - (e1[2] * e2[1]),
             (e1[2] * e2[0]) - (e1[0] * e2[2]),
             (e1[0] * e2[1]) - (e1[1] * e2[0])};
